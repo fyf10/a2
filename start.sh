9abc00399943cbad1c41ceb1ff04604e43d34943
fyf10 <fy00yy@outlook.com>
2025-07-02 15:06:13 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
-#!/bin/sh
-aria2c --conf-path=/etc/aria2/aria2.conf --daemon
-nginx -g "daemon off;"
+#!/bin/bash
+# start.sh
+
+# 启动 aria2c
+aria2c --conf-path=/etc/aria2/aria2.conf &
+
+# 启动简易 HTTP 服务器 (Python3)
+cd /usr/share/nginx/html
+python3 -m http.server 8080
