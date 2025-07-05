f58a0552f5be0f837f247ac1cc69fe1cef0bd42a
fyf10 <fy00yy@outlook.com>
2025-07-05 15:02:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 启动Aria2后台服务
-aria2c --conf-path=/home/aria2/aria2.conf --daemon=true
+#aria2c --conf-path=/home/aria2/aria2.conf --daemon=true
 
 # 启动Nginx前台进程（保持容器运行）
 exec nginx -g 'daemon off;'
